09cf94e85ef7979b7ec14280de338e692b4b07aa
Lucas dos Santos Rodrigues Szavara <szavara.lucas.santos@gmail.com>
2023-09-17 16:47:30 -0300

    Criar paginas

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import html
 
+from layouts.menu import get_menu
 
 # Incluir o Bootstrap
 external_stylesheets = [
@@ -24,18 +25,21 @@ app = dash.Dash(
     __name__,
     external_stylesheets=external_stylesheets,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+    use_pages=True,
+    pages_folder="projeto_covid",
 )
 app.server.config.from_object(__name__)
 
 # Criar uma tela para o app
 app.layout = html.Div(
     [
-        html.Div(
-            [
-                html.Div('oi', id="page-content"),
+        get_menu(
+            menu_id="menu-dash",
+            options=[
+                {"label": page["title"], "value": page["path"]}
+                for page in dash.page_registry.values()
             ],
-            className="row",
         ),
+        dash.page_container,
     ],
-    id="content",
 )
--- index.py
+++ index.py
@@ -2,12 +2,32 @@
 import logging
 import os
 
+import dash
+from dash.dependencies import Input, Output
+
 from app import app
+from projeto_covid.efeitos_regionais import index
+from projeto_covid.visao_geral import index
 
 logging.basicConfig(
     level=os.environ.get("logging_level", "WARNING"),
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
+
+@app.callback(Output("menu-dash", "label"), [Input("_pages_location", "pathname")])
+def get_tela(id_tela: str):
+    """Preenche a div com id `dash-area` com o layout do módulo selecionado.
+
+    Args:
+        id_tela: O id da opção selecionada Ele sempre será igual ao nome da pasta do módulo.
+
+    Returns:
+        O layout do módulo selecionado
+    """
+    for page in dash.page_registry.values():
+        if id_tela == page["relative_path"]:
+            return page["title"]
+    return "Escolha uma tela"
 
 if __name__ == "__main__":
     logging.info("Running")
