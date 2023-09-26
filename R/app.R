85f71a4107094e6ef54cf9ab153bd52a10efcb7a
gustavo-andrade-tavares <guandradetavares@gmail.com>
2023-09-26 13:32:27 -0300

    adição de gráficos

--- R/app.R
+++ R/app.R
@@ -2,6 +2,7 @@
 #install.packages("pacman")
 pacman::p_load(shiny, shinydashboard, shinythemes, COVID19, fpp3, plotly, forecast)
 
+
 #------------------------------------------------------------
 
 # Dados para teste
@@ -58,18 +59,39 @@ ui <- dashboardPage(
               # Adicione elementos específicos para a Página 1 aqui
       ),
       tabItem(tabName = "vis",
-              column(width = 2, 
-                     box(width = NULL, status = "warning", solidHeader = TRUE,
-                         selectInput("e_c", "Estado", est, selectize = TRUE)),
-                     uiOutput('html_filtro_cidade'),
-                     box(width = NULL, status = "warning", solidHeader = TRUE,
-                         sliderInput("date_slider", "Período", min = min(dados_estados$date), max = max(dados_estados$date),
-                                     value = c(min(dados_estados$date), max(dados_estados$date)))),
-                     box(width = NULL, status = "warning", solidHeader = TRUE,
-                         selectInput("inicio", "Data de início do gráfico", inicio))),
-              column(width = 10, 
-                     box(width = NULL, solidHeader = TRUE, 
-                         plotlyOutput("grafico_series", height = 500)))
+              fluidRow(
+                h2(" 1) Confirmados"),
+                column(width = 2, 
+                       box(width = NULL, status = "warning", solidHeader = TRUE,
+                           selectInput("e_c", "Estado", est, selectize = TRUE)),
+                       uiOutput('html_filtro_cidade'),
+                       box(width = NULL, status = "warning", solidHeader = TRUE,
+                           sliderInput("date_slider", "Período", min = min(dados_estados$date), max = max(dados_estados$date),
+                                       value = c(min(dados_estados$date), max(dados_estados$date)))),
+                       box(width = NULL, status = "warning", solidHeader = TRUE,
+                           selectInput("inicio", "Data de início do gráfico", inicio))),
+                column(width = 10, 
+                       box(width = NULL, solidHeader = TRUE, 
+                           plotlyOutput("grafico_series", height = 500)))
+              ),
+              
+              fluidRow(
+                h2(" 2) Mortalidade"),
+                column(width = 2
+                ),
+                column(width = 10, 
+                       box(width = NULL, solidHeader = TRUE, 
+                           plotlyOutput("grafico_series1", height = 500)))
+              ),
+              
+              fluidRow(
+                h2(" 3) Doses de Vacinas administradas"),
+                column(width = 2
+                ),
+                column(width = 10, 
+                       box(width = NULL, solidHeader = TRUE, 
+                           plotlyOutput("grafico_series2", height = 500)))
+              )
       ),
       tabItem("ev",
               h2("Conteúdo da Página 3"),
@@ -101,13 +123,15 @@ server <- function(input, output, session) {
     if(!(is.null(est) || est == '')) {
       cidades <- unique(locais[locais$estados == est, ]$cidades)
       return(box(width = NULL, status = "warning", solidHeader = TRUE,
-          selectInput("cidade_filtro", "Cidade", c('', cidades), selectize = TRUE)))
+                 selectInput("cidade_filtro", "Cidade", c('', cidades), selectize = TRUE)))
     }
   })
   observe({
     est <- input$e_c
     updateSelectInput(session, 'cidade_filtro', selected='')
   })
+  
+  
   output$grafico_series <- renderPlotly({
     est <- input$e_c
     cid <- input$cidade_filtro
@@ -115,15 +139,15 @@ server <- function(input, output, session) {
     print(cid)
     if(is.null(est) || est == '') {
       df_cidade <- covid19(country = c('Brazil'), level=1, verbose=F)  %>% filter(date >= input$date_slider[1],
-                                                                  date <= input$date_slider[2])
+                                                                                  date <= input$date_slider[2])
     } else {
       if(!(is.null(cid) || cid == '') && file.exists(paste('dados/dados', est, cid, '.csv'))) {
-          df_cidade <- read.csv(paste('dados/dados', est, cid, '.csv')) %>% filter(date >= input$date_slider[1],
-                    date <= input$date_slider[2])
+        df_cidade <- read.csv(paste('dados/dados', est, cid, '.csv')) %>% filter(date >= input$date_slider[1],
+                                                                                 date <= input$date_slider[2])
       }
       else {
         df_cidade <- dados_estados %>% filter(administrative_area_level_2 == est) %>% filter(date >= input$date_slider[1],
-                  date <= input$date_slider[2])
+                                                                                             date <= input$date_slider[2])
       }
     }
     df_cidade$date <- as.Date(df_cidade$date)
@@ -141,6 +165,77 @@ server <- function(input, output, session) {
     fig <- ggplotly(p)
     return(fig)
   })
+  
+  
+  output$grafico_series1 <- renderPlotly({
+    est <- input$e_c
+    cid <- input$cidade_filtro
+    print(est)
+    print(cid)
+    if(is.null(est) || est == '') {
+      df_cidade <- covid19(country = c('Brazil'), level=1, verbose=F)  %>% filter(date >= input$date_slider[1],
+                                                                                  date <= input$date_slider[2])
+    } else {
+      if(!(is.null(cid) || cid == '') && file.exists(paste('dados/dados', est, cid, '.csv'))) {
+        df_cidade <- read.csv(paste('dados/dados', est, cid, '.csv')) %>% filter(date >= input$date_slider[1],
+                                                                                 date <= input$date_slider[2])
+      }
+      else {
+        df_cidade <- dados_estados %>% filter(administrative_area_level_2 == est) %>% filter(date >= input$date_slider[1],
+                                                                                             date <= input$date_slider[2])
+      }
+    }
+    df_cidade$date <- as.Date(df_cidade$date)
+    df_cidade <- corrige(df_cidade,"deaths")
+    
+    p <- df_cidade %>%
+      slice(-1) %>%
+      mutate(deaths = diff(df_cidade$deaths)) %>%
+      ggplot(aes(x = date, y = deaths)) +
+      geom_line(color = "red") +
+      labs(title = paste("Casos de mortalidade em", cid, ', ', est), x = "Data", y = "Número de Mortes Diárias") +
+      theme_minimal() +
+      scale_x_date(date_breaks = "4 months", date_labels = "%b-%Y")
+    
+    fig <- ggplotly(p)
+    return(fig)
+  })
+  
+  output$grafico_series2 <- renderPlotly({
+    est <- input$e_c
+    cid <- input$cidade_filtro
+    print(est)
+    print(cid)
+    if(is.null(est) || est == '') {
+      df_cidade <- covid19(country = c('Brazil'), level=1, verbose=F)  %>% filter(date >= input$date_slider[1],
+                                                                                  date <= input$date_slider[2])
+    } else {
+      if(!(is.null(cid) || cid == '') && file.exists(paste('dados/dados', est, cid, '.csv'))) {
+        df_cidade <- read.csv(paste('dados/dados', est, cid, '.csv')) %>% filter(date >= input$date_slider[1],
+                                                                                 date <= input$date_slider[2])
+      }
+      else {
+        df_cidade <- dados_estados %>% filter(administrative_area_level_2 == est) %>% filter(date >= input$date_slider[1],
+                                                                                             date <= input$date_slider[2])
+      }
+    }
+    df_cidade$date <- as.Date(df_cidade$date)
+    df_cidade <- corrige(df_cidade,"vaccines")
+    
+    p <- df_cidade %>%
+      slice(-1) %>%
+      mutate(vaccines = diff(df_cidade$vaccines)) %>%
+      ggplot(aes(x = date, y = vaccines)) +
+      geom_line(color = "green") +
+      labs(title = paste("Doses de vacinas administradas em", cid, ', ', est), x = "Data", y = "Doses de vacinas Diárias") +
+      theme_minimal() +
+      scale_x_date(date_breaks = "4 months", date_labels = "%b-%Y")
+    
+    fig <- ggplotly(p)
+    return(fig)
+  })
+  
+  
 }
 
 #------------------------------------------------------------
